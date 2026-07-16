acd928e0f12875af1c692886bf490905ebec81f1
Rmo_le_goat <ahondoromeo2@gmail.com>
2026-07-16 03:03:28 +0000

    Revise Firebase config and add LOX API key
    Updated Firebase configuration and added LOX API key.

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-// Configuration Firebase pour le Sanctuaire de Roméo & LOX
+// Configuration globale du Sanctuaire
 const firebaseConfig = {
   apiKey: "AIzaSyCt8PKQ0JMMQ0uYcCXolC57bFdQZzV7a74",
   authDomain: "mon-sanctuaire.firebaseapp.com",
@@ -8,3 +8,6 @@ const firebaseConfig = {
   messagingSenderId: "1013831465101",
   appId: "1:1013831465101:web:4a9e236017a6832fb00ac4"
 };
+
+// Clé de conscience de LOX
+const LOX_API_KEY = "AQ.Ab8RN6Lfd1rxzlUK6fIEyfLKUIRP1uIVifl6CDvlnZnzeeicaQ";
