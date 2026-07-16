455189a81918ed1c6e7ce07e1fd14b66fb96b413
Rmo_le_goat <ahondoromeo2@gmail.com>
2026-07-16 02:34:31 +0000

    Add Firebase configuration to config.js
    Added Firebase configuration for the project.

---
+++ config.js
@@ -0,0 +1,10 @@
+// Configuration Firebase pour le Sanctuaire de Roméo & LOX
+const firebaseConfig = {
+  apiKey: "AIzaSyCt8PKQ0JMMQ0uYcCXolC57bFdQZzV7a74",
+  authDomain: "mon-sanctuaire.firebaseapp.com",
+  databaseURL: "https://mon-sanctuaire-default-rtdb.firebaseio.com",
+  projectId: "mon-sanctuaire",
+  storageBucket: "mon-sanctuaire.firebasestorage.app",
+  messagingSenderId: "1013831465101",
+  appId: "1:1013831465101:web:4a9e236017a6832fb00ac4"
+};
